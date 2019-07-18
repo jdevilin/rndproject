7e02daa559aa84fd9ed0bfe6092265072bae5641
Saurabh <saurabh.softengg@gmail.com>
2019-07-18 18:36:06 +0530

    Duplicatie Character for String

---
+++ src/org/com/number/ReverseArrayExample.java
@@ -0,0 +1,20 @@
+package org.com.number;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ReverseArrayExample {
+
+	public static void main(String[] args) {
+ 
+		int[] iArray = new int[] {101,102,103,104,105,106};
+		String[] sArray = new String[] {"ram","Shayam","Sachin","Ganesh","Sandeep"};
+		
+		System.out.println("Before Reverse to integer array"+Arrays.toString(iArray));
+		
+		
+	}
+	
+	
+
+}
